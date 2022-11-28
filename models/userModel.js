d8d8423d49502b9618987e0787c57c23bc540d01
Daniel Lugo <daniellugofreelance@gmail.com>
2022-11-28 18:43:18 -0300

    create User Model 9+ Routes + Controllers

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+
+
+
+const schema = new mongoose.Schema({
+    name : { type: String, required: true},
+    email : {type: String, required: true},
+    password: [{ type: String, required: true}],
+    from: [{ type: String, required: true}],
+    code: {type: String, required: true},
+    role: {type: String, required: true},
+    logged: {type: Boolean, required: true},
+    verified: {type: Boolean, required: true}
+})
+
+
+const USERMODEL = mongoose.model(
+    'users',
+    schema
+)
+
+module.exports = USERMODEL;
